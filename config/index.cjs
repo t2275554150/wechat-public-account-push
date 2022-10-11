c9efedebe22cf1935ac750ea6e3cae80040ed446
苍笙 <50491658+t2275554150@users.noreply.github.com>
2022-10-11 10:00:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'mk9uLrxp-n94uAI_ZXrpZIhVRucX9MAVHt4CNguP0DU',
+      useTemplateId: 'OFxRRq0vA9SJvawBMTA_8f8GgQC8h_EYPa95P5rUmbI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '2-27',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'mk9uLrxp-n94uAI_ZXrpZIhVRucX9MAVHt4CNguP0DU',
+  CALLBACK_TEMPLATE_ID: 'OFxRRq0vA9SJvawBMTA_8f8GgQC8h_EYPa95P5rUmbI',
 
   CALLBACK_USERS: [
     {
