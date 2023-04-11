ebfaeacfc04d6875691d29b7993c4ece6bc98010
苍笙 <50491658+t2275554150@users.noreply.github.com>
2023-04-11 23:46:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o9-OK6PGAcFIAk8D7SsyPJFv8_Gc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'bXsitv3dOmmyBM0NbLsoDuDLqNNwbstIVwfVxESBAG4',
+      useTemplateId: 'rhdaol__GUYLZ5L9eW_GFzg3UT07oKbzGNgk5JlQosA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-08',
       festivals: [
