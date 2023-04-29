adb79ad5d80a66674f35b581752ff16834e22052
苍笙 <50491658+t2275554150@users.noreply.github.com>
2023-04-29 18:31:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -41,7 +41,7 @@ const USER_CONFIG = {
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
-         在一起的日子
+         //在一起的日子
         { keyword: 'love_day', date: '2023-04-16' },
         // 结婚纪念日
        // { keyword: 'marry_day', date: '2022-09-09' },
